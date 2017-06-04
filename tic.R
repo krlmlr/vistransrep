556dc62fa5840caa816750edfbabde2f7f725d92
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-04 21:40:37 +0200

    add github.com to known hosts

--- tic.R
+++ tic.R
@@ -2,5 +2,6 @@ get_stage("script") %>%
   add_step(step_hello_world())
 
 get_stage("deploy") %>%
+  add_step(step_add_to_known_hosts("github.com")) %>%
   add_step(step_install_ssh_keys()) %>%
   add_step(step_test_ssh())
