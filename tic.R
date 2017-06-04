b472a6c77c4acc0509468d81625c50e2fe7e45e1
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-04 21:40:55 +0200

    remove deploy to fill cache on OS X

--- tic.R
+++ tic.R
@@ -1,7 +1,2 @@
 get_stage("script") %>%
   add_step(step_hello_world())
-
-get_stage("deploy") %>%
-  add_step(step_add_to_known_hosts("github.com")) %>%
-  add_step(step_install_ssh_keys()) %>%
-  add_step(step_test_ssh())
