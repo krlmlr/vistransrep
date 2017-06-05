d9d687428f4fb1d92944e247802213b2649bddf0
Kirill Müller <krlmlr+r@mailbox.org>
2017-06-05 19:11:03 +0200

    test it

--- tic.R
+++ tic.R
@@ -1,7 +1,8 @@
 get_stage("script") %>%
-  add_step(step_hello_world())
+  add_step(step_run_code(rmarkdown::build_site()))
 
 get_stage("deploy") %>%
   add_step(step_add_to_known_hosts("github.com")) %>%
   add_step(step_install_ssh_keys()) %>%
-  add_step(step_test_ssh())
+  add_step(step_test_ssh()) %>%
+  add_step(step_push_deploy())
