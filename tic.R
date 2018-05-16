b0d112b23450dfebca07dd5f11a24fd142ad9611
Kirill Müller <krlmlr+r@mailbox.org>
2018-05-16 18:47:42 +0200

    render website on Travis CI

--- tic.R
+++ tic.R
@@ -9,5 +9,5 @@ get_stage("deploy") %>%
   add_step(step_add_to_known_hosts("github.com")) %>%
   add_step(step_install_ssh_keys()) %>%
   add_step(step_test_ssh()) %>%
-  add_step(step_run_code(rmarkdown::render_site())) %>%
+  add_step(step_run_code(withr::with_dir("website", rmarkdown::render_site()))) %>%
   add_step(step_push_deploy())
