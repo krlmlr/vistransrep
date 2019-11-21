91b71f7839d3d942a7bb88fe0cbf7d643a507ff6
Kirill Müller <krlmlr+r@mailbox.org>
2019-11-21 15:16:15 +0100

    Complete joins

--- book/proj/script/61-join.R
+++ book/proj/script/61-join.R
@@ -9,38 +9,43 @@ library(conflicted)
 conflict_prefer("filter", "dplyr")
 conflict_prefer("lag", "dplyr")
 
-
+# airlines contain detailed information for each airline
+# (in this case only the name)
 airlines
 
-flights %>%
-  left_join(airlines)
-
+# flights can be augmented with this detailed information
 flights %>%
   left_join(airlines) %>%
   select(dep_time, carrier, name)
 
+# for "production code", always indicate the columns to join by
 flights %>%
   left_join(airlines, by = "carrier") %>%
   select(dep_time, carrier, name)
 
+# airlines contain detailed information for each airport
 airports
 
-?flights
+try(
+  flights %>%
+    left_join(airports)
+)
 
-flights %>%
-  left_join(airports)
-
+# in this case, explicit "by" columns are required
 flights %>%
   left_join(airports, by = c("dest" = "faa")) %>%
   select(origin, dest, name)
 
+# what about the NA name?
 airports %>%
   filter(faa == "BQN")
 
+# left vs. inner vs. outer joins control what happens in case of mismatches
 flights %>%
   inner_join(airports, by = c("dest" = "faa")) %>%
   select(origin, dest, name)
 
+# before joining, prepare the RHS table(s)
 origin_airports <-
   airports %>%
   select(origin = faa, origin_name = name)
@@ -53,18 +58,19 @@ dest_airports <-
 
 dest_airports
 
+# much easier with the prepared tables
 flights %>%
   left_join(origin_airports) %>%
   left_join(dest_airports) %>%
   select(origin, origin_name, dest, dest_name)
 
+# "by" still recommended
 flights %>%
   left_join(origin_airports, by = "origin") %>%
   left_join(dest_airports, by = "dest") %>%
   select(origin, origin_name, dest, dest_name)
 
-# Keys!
-
+# either LHS or RHS should have a key as part of "by"
 airports %>%
   count(faa)
 
@@ -72,6 +78,7 @@ airports %>%
   count(faa) %>%
   count(n)
 
+# create artificial dataset where airport is not a key
 dup_airports <-
   bind_rows(airports, sample_n(airports, 100))
 
@@ -82,8 +89,21 @@ dup_airports %>%
   count(faa) %>%
   count(n)
 
+# what happens if not key?
 flights %>%
   left_join(dup_airports, by = c("dest" = "faa")) %>%
   select(origin, dest, name)
 
 flights
+
+# why is the following not a key?
+weather
+
+weather %>%
+  count(origin, year, month, day, hour) %>%
+  count(n)
+
+weather %>%
+  count(origin, year, month, day, hour) %>%
+  add_count(n) %>%
+  filter(n > 1)
